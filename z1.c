23adc7e00d1a54bc47571bc25f736988cd0c9873
kenaj30 <71976425+kenaj30@users.noreply.github.com>
2024-10-15 18:37:00 +0200

    Add files via upload

---
+++ z1.c
@@ -0,0 +1,26 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+    int sfd, rc;
+    char buf[256];
+    struct sockaddr_in saddr;
+    struct hostent* addrent;
+
+    addrent = gethostbyname(argv[1]);
+    sfd = socket(PF_INET, SOCK_STREAM, 0);
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(atoi(argv[2]));
+    memcpy(&saddr.sin_addr.s_addr, addrent -> h_addr, addrent -> h_length);
+    connect(sfd, (struct sockaddr*) &saddr, sizeof(saddr));
+    rc = read(sfd, buf, sizeof(buf));
+    write(1, buf, rc);
+    close(sfd);
+
+    return EXIT_SUCCESS;
+}
